ec5318d7f339b85fa5f0715a85216b61399e1afc
allgoodworks <allgoodworks79@gmail.com>
2020-10-22 13:40:09 +0300

    refactor + added highlighting on hover

--- radarchart.js
+++ radarchart.js
@@ -3,13 +3,14 @@
 function radarChart() {
   let SELECTION,
     innerRadius = 10,
-    outerRadius = 150,
+    outerRadius = 200,
     seriesArcHeight = 40,
     margin = 10,
     windowWidth,
     windowHeight,
     width,
     height,
+    highlightOffset = 10,
     colorSeries = d3
       .scaleOrdinal()
       .range([
@@ -22,9 +23,17 @@ function radarChart() {
       ]),
     colorValues = d3.scaleOrdinal().range(["#0d5a9e", "#000000"]),
     y = d3.scaleLinear().range([innerRadius, outerRadius]).domain([0, 4]),
-    sectors,
+    pie = d3
+      .pie()
+      .sort(null)
+      .value(1)
+      .startAngle((-90 * Math.PI) / 180)
+      .endAngle((-90 * Math.PI) / 180 + 2 * Math.PI),
     svg,
-    startAngle = (-90 * Math.PI) / 180,
+    avSlices,
+    rvSlices,
+    seriesSlices,
+    avLabelSlices,
     legend,
     tooltipContent = (d) => {
       let content = `<div class="t-item"><div class="t-head">${d.data.series}</div>`,
@@ -54,8 +63,6 @@ function radarChart() {
     SELECTION = selection.node();
 
     selection.each(function (data) {
-      sectors = data.length;
-
       windowWidth = this.clientWidth;
       windowHeight = this.clientHeight;
 
@@ -83,203 +90,133 @@ function radarChart() {
         .attr("class", "circle")
         .attr("r", y);
 
-      for (let i = 0; i < sectors; i++) {
-        let endAngle =
-          (-90 * Math.PI) / 180 + (2 * Math.PI * (i + 1)) / sectors;
+      // Arc generator for Acquired Values pies
+      const arcAV = d3
+        .arc()
+        .innerRadius(innerRadius)
+        .outerRadius((d) => y(d.data.av));
 
-        // Arc generator for Acquired Values pies
-        const arcAV = d3
-          .arc()
-          .innerRadius(innerRadius)
-          .outerRadius(y(data[i].av))
-          .startAngle(startAngle)
-          .endAngle(endAngle);
-
-        // Arc generator for Series pies
-        const outerLaberArc = d3
-          .arc()
-          .innerRadius(outerRadius)
-          .outerRadius(outerRadius + seriesArcHeight)
-          .startAngle(startAngle)
-          .endAngle(endAngle);
-
-        startAngle = endAngle;
-
-        // Append pies for Acquired Values
-        svg.append("path").attr("class", "arc--av").attr("d", arcAV);
-
-        // Append axes
-        svg
-          .append("path")
-          .attr("d", `M${innerRadius},0 L${outerRadius},0`)
-          .attr("class", "axis")
-          .attr(
-            "transform",
-            () =>
-              `rotate(${(((-90 * Math.PI) / 180 + endAngle) * 180) / Math.PI})`
-          );
-
-        // Append pies for Series
-        svg
-          .append("path")
-          .attr("class", "arc--series")
-          .attr("d", outerLaberArc)
-          .attr("id", `${SELECTION.id}-arc-series-${i}`)
-          .style("fill", () => colorSeries(i));
-
-        // Append Series labels
-        const seriesLabels = svg
-          .append("text")
-          .attr("class", "label--series-arc")
-          .attr("dx", "-0.5em")
-          .attr("dy", "2em");
-
-        seriesLabels
-          .append("textPath")
-          .attr("xlink:href", `#${SELECTION.id}-arc-series-${i}`)
-          .style("text-anchor", "middle")
-          .attr("startOffset", () =>
-            startAngle > (90 * Math.PI) / 180 ? "75%" : "25%"
-          )
-          .text(data[i].series);
-
-        // Handle multiline labels
-        seriesLabels.each(function () {
-          if (/Edge\/\d./i.test(navigator.userAgent)) {
-            // Microsoft Edge calculates getComputedTextLength wrong
-            if (this.getNumberOfChars() * 6 > outerRadius)
-              wrap(d3.select(this), outerRadius);
-          } else if (this.getComputedTextLength() > outerRadius) wrap(d3.select(this), outerRadius);
-        });
-      }
-
-      // Pies for the Required Values should lie on top, so draw them next
-      startAngle = (-90 * Math.PI) / 180;
-      for (let i = 0; i < sectors; i++) {
-        let endAngle =
-          (-90 * Math.PI) / 180 + (2 * Math.PI * (i + 1)) / sectors;
-
-        // Arc generator for Required Values pies
-        let arcRV = d3
-          .arc()
-          .innerRadius(innerRadius)
-          .outerRadius(y(data[i].rv))
-          .startAngle(startAngle)
-          .endAngle(endAngle);
-
-        startAngle = endAngle;
-
-        // Append pies for Required Values
-        svg.append("path").attr("class", "arc--rv").attr("d", arcRV);
-      }
-
-      // Acquired Values should lie on top, so draw them next
-      startAngle = (-90 * Math.PI) / 180;
-      for (let i = 0; i < sectors; i++) {
-        let endAngle =
-          (-90 * Math.PI) / 180 + (2 * Math.PI * (i + 1)) / sectors;
-
-        // Arc generator for Acquired Values
-        let arcValues = d3
-          .arc()
-          .innerRadius(y(data[i].av))
-          .outerRadius(y(data[i].av))
-          .startAngle(startAngle)
-          .endAngle(endAngle);
-
-        startAngle = endAngle;
-
-        // Append helper arc for Acquired Values (used for aligning values along pie)
-        svg
-          .append("path")
-          .attr("class", "arc-av-labels")
-          .attr("d", arcValues)
-          .attr("id", `${SELECTION.id}-arc-av-labels-${i}`);
-
-        // Append labels
-        svg
-          .append("text")
-          .attr("class", "label--arc")
-          .attr("dy", () =>
-            startAngle > (90 * Math.PI) / 180 ? "0.75em" : "-0.25em"
-          )
-          .append("textPath")
-          .attr("xlink:href", `#${SELECTION.id}-arc-av-labels-${i}`)
-          .style("text-anchor", "middle")
-          .attr("startOffset", () =>
-            startAngle > (90 * Math.PI) / 180 ? "75%" : "25%"
-          )
-          .text(data[i].av);
-      }
-
-      // Append tooltip
-      const tooltip = d3
-        .select(SELECTION)
-        .append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-
-      // Append dotted line
-      const dottedLine = svg
-        .append("path")
-        .attr("d", `M${innerRadius},0 L${outerRadius},0`)
-        .attr("class", "dotted-line");
-
-      const pie = d3
-        .pie()
-        .sort(null)
-        .value((d) => d.av);
-
-      startAngle = (-90 * Math.PI) / 180;
-      svg
-        .selectAll(".arc--tooltip")
+      // Append pies for Acquired Values
+      avSlices = svg
+        .selectAll(".arc--av")
         .data(pie(data))
         .enter()
         .append("path")
-        .attr("class", "arc--tooltip")
+        .attr("class", "arc--av")
+        .attr("d", arcAV);
+
+      // Arc generator for Series pies
+      const outerLaberArc = d3
+        .arc()
+        .innerRadius(outerRadius)
+        .outerRadius(outerRadius + seriesArcHeight);
+
+      // Append axes
+      svg
+        .selectAll(".axis")
+        .data(pie(data))
+        .enter()
+        .append("path")
+        .attr("d", `M${innerRadius},0 L${outerRadius},0`)
+        .attr("class", "axis")
         .attr(
-          "d",
-          d3
-            .arc()
-            .innerRadius(0)
-            .outerRadius(outerRadius + seriesArcHeight)
-            .startAngle(() => startAngle)
-            .endAngle((d, i) => {
-              let endAngle =
-                (-90 * Math.PI) / 180 + (2 * Math.PI * (i + 1)) / sectors;
-              startAngle = endAngle;
-              return endAngle;
-            })
+          "transform",
+          (d) =>
+            `rotate(${(((-90 * Math.PI) / 180 + d.endAngle) * 180) / Math.PI})`
+        );
+
+      // Append pies for Series
+      seriesSlices = svg
+        .selectAll(".arc--series")
+        .data(pie(data))
+        .enter()
+        .append("path")
+        .attr("class", "arc--series")
+        .attr("d", outerLaberArc)
+        .attr("id", (d) => `${SELECTION.id}-arc-series-${d.index}`)
+        .style("fill", (d) => colorSeries(d.index));
+
+      // Append Series labels
+      const seriesLabels = svg
+        .selectAll(".label--series-arc")
+        .data(pie(data))
+        .enter()
+        .append("text")
+        .attr("class", "label--series-arc")
+        .attr("dx", "-0.5em")
+        .attr("dy", "2em");
+
+      seriesLabels
+        .append("textPath")
+        .attr("xlink:href", (d) => `#${SELECTION.id}-arc-series-${d.index}`)
+        .style("text-anchor", "middle")
+        .attr("startOffset", (d) =>
+          d.startAngle > (90 * Math.PI) / 180 ? "75%" : "25%"
         )
-        .on("mouseover", function (e, d) {
-          const c = d3.pointer(e);
+        .text((d) => d.data.series);
 
-          dottedLine.style("visibility", "visible");
-          dottedLine.attr("d", `M0,0 L${c[0]},${c[1]}`);
-          dottedLine.raise();
+      // Handle multiline labels
+      seriesLabels.each(function () {
+        if (/Edge\/\d./i.test(navigator.userAgent)) {
+          // Microsoft Edge calculates getComputedTextLength wrong
+          if (this.getNumberOfChars() * 6 > outerRadius)
+            wrap(d3.select(this), outerRadius);
+        } else if (this.getComputedTextLength() > outerRadius) wrap(d3.select(this), outerRadius);
+      });
 
-          tooltip.transition().duration(200).style("opacity", 0.9);
+      // Pies for the Required Values should lie on top, so draw them next
 
-          tooltip
-            .html(tooltipContent(d))
-            .style("left", `${e.pageX + 10}px`)
-            .style("top", `${e.pageY}px`);
-        })
-        .on("mousemove", function (e, d) {
-          const c = d3.pointer(e);
+      // Arc generator for Required Values pies
+      let arcRV = d3
+        .arc()
+        .innerRadius(innerRadius)
+        .outerRadius((d) => y(d.data.rv));
 
-          dottedLine.attr("d", `M0,0 L${c[0]},${c[1]}`);
+      // Append pies for Required Values
+      rvSlices = svg
+        .selectAll(".arc--rv")
+        .data(pie(data))
+        .enter()
+        .append("path")
+        .attr("class", "arc--rv")
+        .attr("d", arcRV);
 
-          tooltip
-            .html(tooltipContent(d))
-            .style("left", `${e.pageX + 10}px`)
-            .style("top", `${e.pageY}px`);
-        })
-        .on("mouseout", function () {
-          dottedLine.style("visibility", "hidden");
+      // Acquired Values should lie on top, so draw them next
 
-          tooltip.transition().duration(500).style("opacity", 0);
-        });
+      // Arc generator for Acquired Values
+      let arcValues = d3
+        .arc()
+        .innerRadius((d) => y(d.data.av))
+        .outerRadius((d) => y(d.data.av));
+
+      // Append pies for Required Values
+      // Append helper arc for Acquired Values (used for aligning values along pie)
+      avLabelSlices = svg
+        .selectAll(".arc--av-labels")
+        .data(pie(data))
+        .enter()
+        .append("path")
+        .attr("class", "arc--av-labels")
+        .attr("d", arcValues)
+        .attr("id", (d) => `${SELECTION.id}-arc--av-labels-${d.index}`);
+
+      // Append labels
+      svg
+        .selectAll(".label--arc")
+        .data(pie(data))
+        .enter()
+        .append("text")
+        .attr("class", "label--arc")
+        .attr("dy", (d) =>
+          d.startAngle > (90 * Math.PI) / 180 ? "0.75em" : "-0.25em"
+        )
+        .append("textPath")
+        .attr("xlink:href", (d) => `#${SELECTION.id}-arc--av-labels-${d.index}`)
+        .style("text-anchor", "middle")
+        .attr("startOffset", (d) =>
+          d.startAngle > (90 * Math.PI) / 180 ? "75%" : "25%"
+        )
+        .text((d) => d.data.av);
 
       // Append Legend
       const legendG = svg.append("g").attr("class", "legend");
@@ -345,6 +282,130 @@ function radarChart() {
             outerRadius + seriesArcHeight * 2 - margin
           })`
       );
+
+      // Append tooltip
+      const tooltip = d3
+        .select(SELECTION)
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+      // Append dotted line
+      const dottedLine = svg
+        .append("path")
+        .attr("d", `M${innerRadius},0 L${outerRadius},0`)
+        .attr("class", "dotted-line");
+
+      // Arc generator for tooltips
+      let arcTooltip = d3
+        .arc()
+        .innerRadius(0)
+        .outerRadius(outerRadius + seriesArcHeight);
+
+      svg
+        .selectAll(".arc--tooltip")
+        .data(pie(data))
+        .enter()
+        .append("path")
+        .attr("class", "arc--tooltip")
+        .attr("d", arcTooltip)
+        .on("mouseover", function (e, d) {
+          let arcAV_highlighted = d3
+            .arc()
+            .innerRadius(innerRadius + highlightOffset)
+            .outerRadius(y(d.data.av) + highlightOffset);
+
+          avSlices
+            .filter((k, i) => d.index === i)
+            .transition()
+            .duration(200)
+            .attr("d", arcAV_highlighted);
+
+          let arcValues_highlighted = d3
+            .arc()
+            .innerRadius((d) => y(d.data.av) + highlightOffset)
+            .outerRadius((d) => y(d.data.av) + highlightOffset);
+
+          avLabelSlices
+            .filter((k, i) => d.index === i)
+            .transition()
+            .duration(200)
+            .attr("d", arcValues_highlighted);
+
+          let arcRV_highlighted = d3
+            .arc()
+            .innerRadius(innerRadius + highlightOffset)
+            .outerRadius((d) => y(d.data.rv) + highlightOffset);
+
+          rvSlices
+            .filter((k, i) => d.index === i)
+            .transition()
+            .duration(200)
+            .attr("d", arcRV_highlighted);
+
+          let outerLaberArc_highlighted = d3
+            .arc()
+            .innerRadius(outerRadius + 10)
+            .outerRadius(outerRadius + seriesArcHeight + 10);
+
+          seriesSlices
+            .filter((k, i) => d.index === i)
+            .transition()
+            .duration(200)
+            .attr("d", outerLaberArc_highlighted);
+
+          const c = d3.pointer(e);
+
+          dottedLine.style("visibility", "visible");
+          dottedLine.attr("d", `M0,0 L${c[0]},${c[1]}`);
+          dottedLine.raise();
+
+          tooltip.transition().duration(200).style("opacity", 0.9);
+
+          tooltip
+            .html(tooltipContent(d))
+            .style("left", `${e.pageX + 10}px`)
+            .style("top", `${e.pageY}px`);
+        })
+        .on("mousemove", function (e, d) {
+          const c = d3.pointer(e);
+
+          dottedLine.attr("d", `M0,0 L${c[0]},${c[1]}`);
+
+          tooltip
+            .html(tooltipContent(d))
+            .style("left", `${e.pageX + 10}px`)
+            .style("top", `${e.pageY}px`);
+        })
+        .on("mouseout", function (e, d) {
+          avSlices
+            .filter((k, i) => d.index === i)
+            .transition()
+            .duration(200)
+            .attr("d", arcAV);
+
+          avLabelSlices
+            .filter((k, i) => d.index === i)
+            .transition()
+            .duration(200)
+            .attr("d", arcValues);
+
+          rvSlices
+            .filter((k, i) => d.index === i)
+            .transition()
+            .duration(200)
+            .attr("d", arcRV);
+
+          seriesSlices
+            .filter((k, i) => d.index === i)
+            .transition()
+            .duration(200)
+            .attr("d", outerLaberArc);
+
+          dottedLine.style("visibility", "hidden");
+
+          tooltip.transition().duration(500).style("opacity", 0);
+        });
     });
   }
 
